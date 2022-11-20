207dcc035e4fbfdc932ec91bef1a58769aab0844
Janusz Dalecki <rejkid@optusnet.com.au>
2022-11-21 02:31:29 +1100

    g

--- Program.cs
+++ Program.cs
@@ -14,9 +14,9 @@ namespace WebApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>()
-                        //.UseUrls("https://192.168.0.19:4000");
-                        .UseUrls("http://192.168.0.19:4000");
+                    webBuilder.UseStartup<Startup>();
+                        //.UseUrls("https://192.168.0.19:4000")
+                        //.UseUrls("http://192.168.0.19:4000");
                         //.UseUrls("http://localhost:4000");
                         //.UseUrls("https://webapijanusz.azurewebsites.net:4000");
                 });
