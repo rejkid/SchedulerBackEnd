2e5fd26d55ddd4c0afaf24b72ba30d859fccd0bc
Janusz Dalecki <rejkid@optusnet.com.au>
2022-11-21 16:15:02 +1100

    a

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -23,19 +23,26 @@ namespace WebApi.Services
 
         public void Send(string to, string subject, string html, string from = null)
         {
-            // create message
-            var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse(from ?? _appSettings.EmailFrom));
-            email.To.Add(MailboxAddress.Parse(to));
-            email.Subject = subject;
-            email.Body = new TextPart(TextFormat.Html) { Text = html };
+            try
+            {
+                // create message
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse(from ?? _appSettings.EmailFrom));
+                email.To.Add(MailboxAddress.Parse(to));
+                email.Subject = subject;
+                email.Body = new TextPart(TextFormat.Html) { Text = html };
 
-            // send email
-            using var smtp = new SmtpClient();
-            smtp.Connect(_appSettings.SmtpHost, _appSettings.SmtpPort, SecureSocketOptions.StartTls);
-            smtp.Authenticate(_appSettings.SmtpUser, _appSettings.SmtpPass);
-            smtp.Send(email);
-            smtp.Disconnect(true);
+                // send email
+                using var smtp = new SmtpClient();
+                smtp.Connect(_appSettings.SmtpHost, _appSettings.SmtpPort, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_appSettings.SmtpUser, _appSettings.SmtpPass);
+                smtp.Send(email);
+                smtp.Disconnect(true);
+            }
+            catch (System.Exception ex)
+            {
+                throw new AppException("There was a problem sending e-mail.") ;
+            }
         }
     }
 }
