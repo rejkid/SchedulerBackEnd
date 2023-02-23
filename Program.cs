7cdc8893e9cea3c1c364b6b5cb47bd3884226a71
Janusz Dalecki <rejkid@optusnet.com.au>
2023-02-23 20:30:19 +1100

    Added semicolon

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace WebApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                        //.UseUrls("https://192.168.0.19:4000")
+                        //.UseUrls("https://192.168.0.19:4000");
                         //.UseUrls("http://192.168.0.19:4000");
                         //.UseUrls("http://localhost:4000");
                         //.UseUrls("https://webapijanusz.azurewebsites.net:4000");
